ba07f083fb0a341cb2888b6f52fa296e4da3b827
DiLP <dikingandroid@gmail.com>
2024-08-26 00:36:41 -0700

    change springBootApplication name

--- src/main/java/mockProject/team3/Vaccination_20/Team3VaccinationApplication.java
+++ src/main/java/mockProject/team3/Vaccination_20/Team3VaccinationApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Application {
+public class Team3VaccinationApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+		SpringApplication.run(Team3VaccinationApplication.class, args);
 	}
 
 }
