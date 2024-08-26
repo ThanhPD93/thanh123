c4c73f45cc4af5fe12bd9544dbbe6d9eba9b69d7
DiLP <dikingandroid@gmail.com>
2024-08-26 00:28:12 -0700

    repair git repository

---
+++ src/main/java/mockProject/team3/Vaccination_20/Application.java
@@ -0,0 +1,13 @@
+package mockProject.team3.Vaccination_20;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ src/test/java/mockProject/team3/Vaccination_20/ApplicationTests.java
@@ -0,0 +1,13 @@
+package mockProject.team3.Vaccination_20;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
