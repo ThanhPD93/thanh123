e105738a0ef522abd781172c62a671393e0bfbdb
DiLP <dikingandroid@gmail.com>
2024-08-26 23:56:03 -0700

    revise name due to typo

--- src/main/java/mockProject/team3/Vaccination_20/repository/InjectionSchedule.java
+++
@@ -1,6 +0,0 @@
-package mockProject.team3.Vaccination_20.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface InjectionSchedule extends JpaRepository<InjectionSchedule, String> {
-}
---
+++ src/main/java/mockProject/team3/Vaccination_20/repository/InjectionScheduleRepository.java
@@ -0,0 +1,7 @@
+package mockProject.team3.Vaccination_20.repository;
+
+import mockProject.team3.Vaccination_20.model.InjectionSchedule;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface InjectionScheduleRepository extends JpaRepository<InjectionSchedule, String> {
+}
