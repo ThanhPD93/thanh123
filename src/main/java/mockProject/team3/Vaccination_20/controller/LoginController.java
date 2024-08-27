481c4e4d654f2cf3345fdcc7c8c5b8f519b00272
VinhNguyen300197 <vinhnguyen300197@gmail.com>
2024-08-27 14:16:47 +0700

    add LoginController

---
+++ src/main/java/mockProject/team3/Vaccination_20/controller/LoginController.java
@@ -0,0 +1,18 @@
+package mockProject.team3.Vaccination_20.controller;
+
+import mockProject.team3.Vaccination_20.model.Employee;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class LoginController {
+
+    @GetMapping("login")
+    public String loginPage(Model model){
+        model.addAttribute("employee", new Employee());
+        return "MemberLogin";
+    }
+}
