32cf5dc28e7f5c64a6a582f5cb10bbe200cfa39a
ThanhPD <doanthanh1999@gmail.com>
2024-09-12 15:44:35 +0700

    Vaccine Type Management ( add new, make in-active, update by code(id), search and list with pagination. Almost done!

---
+++ src/main/java/mockProject/team3/Vaccination_20/dto/vaccineTypeDto/CRequestVaccineType.java
@@ -0,0 +1,21 @@
+package mockProject.team3.Vaccination_20.dto.vaccineTypeDto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class CRequestVaccineType {
+
+    private String vaccineTypeId;
+
+    private String vaccineTypeName;
+
+    private String vaccineTypeDescription;
+
+    private String vaccineTypeImage;
+
+    private String status;
+}
--- src/main/java/mockProject/team3/Vaccination_20/service/VaccineTypeService.java
+++ src/main/java/mockProject/team3/Vaccination_20/service/VaccineTypeService.java
@@ -1,9 +1,25 @@
 package mockProject.team3.Vaccination_20.service;
 
+import mockProject.team3.Vaccination_20.dto.vaccineTypeDto.CRequestVaccineType;
+import mockProject.team3.Vaccination_20.dto.vaccineTypeDto.DResponseVaccineType;
 import mockProject.team3.Vaccination_20.model.VaccineType;
+import org.springframework.data.domain.Page;
 
 import java.util.List;
 
 public interface VaccineTypeService {
     List<VaccineType> getAllVaccineTypes();
+
+    VaccineType addVaccineType(VaccineType vaccineType);
+
+    VaccineType findVaccineTypeById(String id);
+
+    DResponseVaccineType addVaccineType(CRequestVaccineType cRequestVaccineType);
+
+    VaccineType findById(String id);
+
+    int makeInactive(List<String> vaccineTypeIds);
+
+    Page<VaccineType> findBySearchWithPagination(String searchInput, int page, int size);
+
 }
