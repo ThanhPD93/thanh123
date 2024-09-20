b163a525e783371d71a2364090825b69a645ef92
VinhNguyen300197 <vinhnguyen300197@gmail.com>
2024-09-20 09:49:09 +0700

    create service for news

--- src/main/java/mockProject/team3/Vaccination_20/service/NewsService.java
+++ src/main/java/mockProject/team3/Vaccination_20/service/NewsService.java
@@ -1,4 +1,11 @@
 package mockProject.team3.Vaccination_20.service;
 
+import mockProject.team3.Vaccination_20.dto.newsDto.NewsAddRequestDto;
+import mockProject.team3.Vaccination_20.dto.newsDto.NewsResponseDto;
+import org.springframework.data.domain.Page;
+
 public interface NewsService {
+    NewsResponseDto addNews(NewsAddRequestDto newsAddRequestDto);
+
+    public Page<NewsResponseDto> findByTittleOrContent(String searchInput, int page, int size);
 }
