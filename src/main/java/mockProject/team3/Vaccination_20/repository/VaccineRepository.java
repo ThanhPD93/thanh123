a25ace1d7f7d28b2801005d7233efcd5b4968a1f
VinhNguyen300197 <vinhnguyen300197@gmail.com>
2024-09-24 14:12:03 +0700

    add feature export excel template for file import

--- src/main/java/mockProject/team3/Vaccination_20/repository/VaccineRepository.java
+++ src/main/java/mockProject/team3/Vaccination_20/repository/VaccineRepository.java
@@ -1,17 +1,21 @@
 package mockProject.team3.Vaccination_20.repository;
 
+import mockProject.team3.Vaccination_20.dto.vaccineDto.VaccineResponseDto5;
 import mockProject.team3.Vaccination_20.model.Vaccine;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Repository
 public interface VaccineRepository extends JpaRepository<Vaccine, String> {
     Vaccine findByVaccineId(String vaccineName);
+
 
     //use for add-ir
     List<Vaccine> findAll();
@@ -23,4 +27,18 @@ public interface VaccineRepository extends JpaRepository<Vaccine, String> {
             "LOWER(v.vaccineName) LIKE LOWER(CONCAT('%', :searchInput, '%')) OR " +
             "LOWER(v.vaccineType.vaccineTypeName) LIKE LOWER(CONCAT('%', :searchInput, '%'))")
     Page<Vaccine> findBySearchInput(String searchInput, Pageable pageable);
+
+    @Query("SELECT v FROM Vaccine v WHERE " +
+            "(:beginDate IS NULL OR :endDate IS NULL OR " +
+            "(v.timeBeginNextInjection BETWEEN :beginDate AND :endDate) " +
+            "AND (v.timeEndNextInjection BETWEEN :beginDate AND :endDate)) " +
+            "AND (:vaccineTypeName IS NULL OR LOWER(v.vaccineType.vaccineTypeName) = LOWER(:vaccineTypeName)) " +
+            "AND (:origin IS NULL OR LOWER(v.vaccineOrigin) LIKE LOWER(CONCAT('%', :origin, '%')))")
+    Page<Vaccine> findByFilterForReport(@Param("beginDate") LocalDate beginDate,
+                                        @Param("endDate") LocalDate endDate,
+                                        @Param("vaccineTypeName") String vaccineTypeName,
+                                        @Param("origin") String origin,
+                                        Pageable pageable);
+
+
 }
