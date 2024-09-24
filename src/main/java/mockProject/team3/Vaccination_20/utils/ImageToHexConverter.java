319e515cc125ff5f1b6fce8ad85d3e5ec0a8a929
ThanhPD <doanthanh1999@gmail.com>
2024-09-24 14:51:32 +0700

    Customer Report not done yet,

---
+++ src/main/java/mockProject/team3/Vaccination_20/utils/ImageToHexConverter.java
@@ -0,0 +1,20 @@
+//package mockProject.team3.Vaccination_20.utils;
+//
+//import javax.xml.bind.DatatypeConverter;
+//import java.io.File;
+//import java.io.FileInputStream;
+//import java.io.IOException;
+//
+//public class ImageToHexConverter {
+//
+//    public static String convertToHex(String imagePath) throws IOException {
+//        File file = new File(imagePath);
+//        FileInputStream fis = new FileInputStream(file);
+//        byte[] bytes = new byte[(int) file.length()];
+//        fis.read(bytes);
+//        fis.close();
+//        return DatatypeConverter.printHexBinary(bytes);
+//    }
+//}
+
+
